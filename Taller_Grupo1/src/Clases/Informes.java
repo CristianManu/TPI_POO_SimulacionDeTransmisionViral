4d59e160c50c7a9ddd54492d7206340283aa64bf
cristian <cristian@Cristian-PC>
2020-10-17 15:09:59 -0300

    Agregados metodos para añadir y quitar elementos a las listas. Agregado el metodo mostrarInfoActual() Agregados metodos get

--- Taller_Grupo1/src/Clases/Informes.java
+++ Taller_Grupo1/src/Clases/Informes.java
@@ -21,21 +21,73 @@ public class Informes {
     List<Persona> personasCuidadoMedio = new ArrayList<>();
     List<Persona> personasCuidadoBajo = new ArrayList<>();
 
-   
-    public String mostrarInfoActual(){return null;}
+    public void añadirPersonaContagiada(Persona p){
+        personasContagiadas.add(p);
+    }
     
-    public List<Persona> mostrarPersonasContagiadas(){return null;}
+    public void añadirPersonaSana(Persona p){
+        personasSanas.add(p);
+    }
     
-    public List<Persona> mostrarPersonasCuidadoAlto(){return null;}
+    public void añadirPersonaCuidadoAlto(Persona p){
+        personasCuidadoAlto.add(p);
+    }
     
-    public List<Persona> mostrarPersonasCuidadoMedio(){return null;}
+    public void añadirPersonaCuidadoMedio(Persona p){
+        personasCuidadoMedio.add(p);
+    }
     
-    public List<Persona> mostrarPersonasCuidadoBajo(){return null;}
+    public void añadirPersonaCuidadoBajo(Persona p){
+        personasCuidadoBajo.add(p);
+    }
     
+    public void quitarPersonaContagiada(Persona p){
+        personasContagiadas.remove(p);
+    }
     
+    public void quitarPersonaSana(Persona p){
+        personasSanas.remove(p);
+    }
     
+    public void quitarPersonaCuidadoAlto(Persona p){
+        personasCuidadoAlto.remove(p);
+    }
     
+    public void quitarPersonaCuidadoMedio(Persona p){
+        personasCuidadoMedio.remove(p);
+    }
     
+    public void quitarPersonaCuidadoBajo(Persona p){
+        personasCuidadoBajo.remove(p);
+    }
+    public String mostrarInfoActual(){
+        String titulo = "Informe de la situacion" + "\n";
+        String nPerSanas = "Numero de personas Sanas: " + this.getPersonasSanas().size() + "\n";
+        String nPerConta = "Numero de personas Contagiadas: " + this.getPersonasContagiadas().size() + "\n";
+        String nPerCuiAlto = "Numero de personas con Cuidados Altos: " + this.getPersonasCuidadoAlto().size() + "\n";
+        String nPerCuiMedio = "Numero de personas con Cuidados Medios: " + this.getPersonasCuidadoMedio().size() + "\n";
+        String nPerCuiBajo = "Numero de personas con Cuidados Bajos: " + this.getPersonasCuidadoBajo().size() + "\n";
+        return titulo + nPerSanas + nPerConta + nPerCuiAlto + nPerCuiMedio + nPerCuiBajo;
+    }
     
+    public List<Persona> getPersonasSanas(){
+        return this.personasSanas;
+    }
+    
+    public List<Persona> getPersonasContagiadas(){
+        return this.personasContagiadas;
+    }
+    
+    public List<Persona> getPersonasCuidadoAlto(){
+        return this.personasCuidadoAlto;
+    }
+    
+    public List<Persona> getPersonasCuidadoMedio(){
+        return this.personasCuidadoMedio;
+    }
+    
+    public List<Persona> getPersonasCuidadoBajo(){
+        return this.personasCuidadoBajo;
+    }
     
 }
